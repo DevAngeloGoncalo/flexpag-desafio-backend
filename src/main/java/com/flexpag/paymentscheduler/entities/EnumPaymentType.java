c61a453f7dc9df9ded24f4afd8bd23b923449c1e
DevAngeloGoncalo <angelo.goncalo87@gmail.com>
2023-03-06 23:18:21 -0300

    Criação do EnumPaymentType

---
+++ src/main/java/com/flexpag/paymentscheduler/entities/EnumPaymentType.java
@@ -0,0 +1,12 @@
+package com.flexpag.paymentscheduler.entities;
+
+public enum EnumPaymentType {
+    
+    CartaoDeCredito,
+    CartaoDeDebito,
+    Boleto,
+    Pix,
+    Transferencia,
+    Outro;
+
+}
